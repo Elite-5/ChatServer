10660a78540195741458c501ceade431022fe177
Kshitij <79253490+Kshitij1818@users.noreply.github.com>
2021-07-02 10:15:17 +0530

    Update MyFile.java

--- Cygnus/src/Cygnus/MyFile.java
+++ Cygnus/src/Cygnus/MyFile.java
@@ -1,48 +1,52 @@
+//BENNETT UNIVERSITY
+//Elite-5
+//Sanyam Bothra,Sneha Gupta,Ishita Goyal,Kshitij Gupta and Tanishq Agarwal.
+//This class is a supplement of servertransfer class, used to store and access file.
 package Cygnus;
 
-public class MyFile {
+public class MyFile {//class name.
 
     private int id;
     private String name;
     private byte[] data;
     private String fileExtension;
 
-    public MyFile(int id, String name, byte[] data, String fileExtension) {
+    public MyFile(int id, String name, byte[] data, String fileExtension) {//initialising variables.
         this.id = id;
         this.name = name;
         this.data = data;
         this.fileExtension = fileExtension;
     }
 
-    public void setId(int id) {
+    public void setId(int id) {//setting is.
         this.id = id;
     }
 
-    public void setName(String name) {
+    public void setName(String name) {//setting name.
         this.name = name;
     }
 
-    public void setData(byte[] data) {
+    public void setData(byte[] data) {//setting file in form of bytes.
         this.data = data;
     }
 
-    public void setFileExtension(String fileExtension) {
+    public void setFileExtension(String fileExtension) {//setting file extension.
         this.fileExtension = fileExtension;
     }
 
-    public int getId() {
+    public int getId() {//getting id.
         return id;
     }
 
-    public String getName() {
+    public String getName() {//getting name.
         return name;
     }
 
-    public byte[] getData() {
+    public byte[] getData() {//getting data in form of bytes.
         return data;
     }
 
-    public String getFileExtension() {
+    public String getFileExtension() {//getting file extension.
         return fileExtension;
     }
 }
